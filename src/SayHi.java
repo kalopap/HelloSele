e920beb9f624298a0ebb015603ddc1fd0076097c
kalyani <reachkalyanihere@hotmail.com>
2024-03-15 13:19:25 -0500

    removed an unused import

--- src/SayHi.java
+++ src/SayHi.java
@@ -4,7 +4,6 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.safari.SafariDriver;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
